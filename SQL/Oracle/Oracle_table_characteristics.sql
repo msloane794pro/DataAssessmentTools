36aa5da1c8e5fb09f3f19616ebf564d8c38d8ec1
Michael Sloane <michael.sloane@inl.gov>
2024-06-21 15:22:30 -0600

    Updated Oracle Table Char query for PK/FK functionality.

--- SQL/Oracle/Oracle_table_characteristics.sql
+++ SQL/Oracle/Oracle_table_characteristics.sql
@@ -1,15 +1,42 @@
-SELECT table_name as "TableName", 
-       column_name as "ColumnName",         
+SELECT atc.table_name as "TableName", 
+       atc.column_name as "ColumnName",
        CASE 
-           WHEN NVL(data_length, 0) = 0 
-           THEN 'NULL' 
-           ELSE data_type || '(' || data_length || ')'
+           WHEN NVL(atc.data_length, 0) = 0 THEN 'NULL' 
+           ELSE atc.data_type || '(' || atc.data_length || ')'
        END as "SourceDataType",
-       data_type as "DataType", 
-       data_length as "Length"
-FROM all_tab_columns
-WHERE owner = 'DRAWING'  -- Change this value to restrict the list of tables to a specific schema.
-    AND table_name in (
-        SELECT table_name FROM all_tables
-        )
-ORDER BY TABLE_NAME;
+       atc.data_type as "DataType", 
+       atc.data_length as "Length",
+       CASE 
+           WHEN pk_col.column_name IS NOT NULL THEN 'True' 
+           ELSE 'False'
+       END as "IsPrimaryKey",
+       COALESCE(pk_col.constraint_name, '') as "PK_name",
+       COALESCE(pk_col.position, 0) as "PK_ordinal_position",
+       CASE 
+           WHEN fk_info.constraint_name IS NOT NULL THEN 'True' 
+           ELSE 'False'
+       END as "IsForeignKey",
+       COALESCE(fk_info.constraint_name, '') as "FK_name",
+       COALESCE(fk_info.r_table_name, '') as "FK_referenced_table",
+       COALESCE(fk_info.r_column_name, '') as "FK_referenced_column"
+FROM all_tab_columns atc
+LEFT JOIN (
+    SELECT acc.table_name, acc.column_name, ac.constraint_name, acc.position
+    FROM all_cons_columns acc
+    JOIN all_constraints ac ON acc.owner = ac.owner AND acc.constraint_name = ac.constraint_name
+    WHERE ac.constraint_type = 'P'
+      AND ac.owner = 'DRAWING'
+) pk_col ON atc.table_name = pk_col.table_name AND atc.column_name = pk_col.column_name
+LEFT JOIN (
+    SELECT acc.table_name, acc.column_name, ac.constraint_name, 
+           r_ac.table_name AS r_table_name, r_acc.column_name AS r_column_name
+    FROM all_cons_columns acc
+    JOIN all_constraints ac ON acc.owner = ac.owner AND acc.constraint_name = ac.constraint_name
+    JOIN all_constraints r_ac ON ac.r_owner = r_ac.owner AND ac.r_constraint_name = r_ac.constraint_name
+    JOIN all_cons_columns r_acc ON r_ac.owner = r_acc.owner AND r_ac.constraint_name = r_acc.constraint_name AND r_acc.position = acc.position
+    WHERE ac.constraint_type = 'R'
+      AND ac.owner = 'DRAWING'
+) fk_info ON atc.table_name = fk_info.table_name AND atc.column_name = fk_info.column_name
+WHERE atc.OWNER = 'DRAWING'
+  AND atc.table_name IN ('A_DRAFTERS', 'A_DRAWINGS', 'ARP_REPORT', 'CLOSED_DARS', 'DRAFTERS', 'DRAW_NUMBERING', 'DRAWINGS', 'DRAWINGS_AUDIT', 'ENGINEERS', 'ERROR_LOG', 'ESSENTIAL_AREA', 'ESSENTIAL_LOG', 'INDEX_CODES', 'MARKUPTAB', 'OLD_CAD', 'SPECIAL_CODES', 'SPECIAL_CODES_LOG', 'STW_DRAWINGS', 'SUPERSEDED_DRAWINGS', 'SYSTEM_CODES', 'SYSTEM_LOV')
+ORDER BY atc.table_name, atc.column_name;
