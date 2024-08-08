bc98be509407247baff27c0cac380876d19d9292
Michael Sloane <michael.sloane@inl.gov>
2024-08-08 15:00:40 -0600

    Updated Description prompt.

--- Python/DDPhraseGen/TestOpenAiDDPharaseGen.py
+++ Python/DDPhraseGen/TestOpenAiDDPharaseGen.py
@@ -9,17 +9,17 @@ pg = OpenAiDDPhraseGen()
 
 pg.greet()
 
-text = pg.createSessionMessageText('abcdefghijkl', PhraseType.DESCRIPTION, 'ThisTableA', ['Item 1', 'Item 2', 'Item 3'])
-print(text)
+text = pg.createSessionMessageText('abcdefghijkl', PhraseType.DESCRIPTION.name, 'Drawings', 'ThisTableA', ['Item 1', 'Item 2', 'Item 3'])
+print(f'test Case 01:\n{text}\n\n')
 
-text = pg.createSessionMessageText('abcdefghijkl', PhraseType.DESCRIPTION, 'ThisTableB', ['Item 4', 'Item 5', 'Item 6'])
-print(text)
+text = pg.createSessionMessageText('abcdefghijkl', PhraseType.DESCRIPTION.name, 'Documents', 'ThisTableB', ['Item 4', 'Item 5', 'Item 6'])
+print(f'test Case 02:\n{text}\n\n')
 
-text = pg.createSessionMessageText('zyxwbillyraw', PhraseType.DESCRIPTION, 'ThisTableV', ['Item 7', 'Item 8', 'Item 9'])
-print(text)
+text = pg.createSessionMessageText('zyxwbillyraw', PhraseType.DESCRIPTION.name, 'Tracking', 'ThisTableV', ['Item 7', 'Item 8', 'Item 9'])
+print(f'test Case 03:\n{text}\n\n')
 
-text = pg.createSessionMessageText('', PhraseType.DESCRIPTION, 'ThisTableZ', ['Item A', 'Item B', 'Item C'])
-print(text)
+text = pg.createSessionMessageText('', PhraseType.DESCRIPTION.name, 'Work Flow', 'ThisTableZ', ['Item A', 'Item B', 'Item C'])
+print(f'test Case 04:\n{text}\n\n')
 
-text = pg.createSessionMessageText('aeiousometimesy', PhraseType.FRIENDLYNAME, 'ThisTableA', ['Item 1', 'Item 2', 'Item 3'])
-print(text)
+text = pg.createSessionMessageText('aeiousometimesy', PhraseType.FRIENDLYNAME.name, '', 'ThisTableA', ['Item 1', 'Item 2', 'Item 3'])
+print(f'test Case 05:\n{text}\n\n')
