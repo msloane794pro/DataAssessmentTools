2d1bca0cab6551157a102424864cab27c6df8e70
Sara Mount <Sara.Mount@inl.gov>
2024-09-24 07:27:14 -0600

    Add unique constraint

--- SQL/Oracle/Oracle_indexes.sql
+++ SQL/Oracle/Oracle_indexes.sql
@@ -7,7 +7,8 @@ FROM
     all_ind_columns icol
 WHERE 
     icol.table_owner = 'TRAIN' -- Replace with your schema name
-    AND icol.index_name LIKE '%PK%' --Specify special identifier for indexes acting as Primary Key
+    AND idx.uniqueness = 'UNIQUE'
+    AND icol.index_name LIKE '%PK%' --Specify special identifier for indexes acting as Primary Key, as needed
 ORDER BY 
     icol.table_name, 
     icol.index_name, 
